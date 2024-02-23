6ec0f5f7439c829060e5d1dc414d0d7c34cd475a
Nacherpro02 <nacheruddin22@gmail.com>
2024-02-23 15:45:28 +0100

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -3,5 +3,5 @@ function returnReverseString(){
     for(var i = this.length - 1; i >= 0; i--){
         reverseString += this[i];
     }
-    return reverseString;
+    console.log("reverseString");
 }
